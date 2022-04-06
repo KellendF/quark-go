2dc0982f25db0f21e439af10ef2eb87826e67e12
tangtanglove <dai_hang_love@126.com>
2022-04-06 08:41:35 +0800

    wip

--- internal/admin/metrics/team_info.go
+++ internal/admin/metrics/team_info.go
@@ -25,7 +25,7 @@ func (p *TeamInfo) Calculate() *descriptions.Component {
 	return p.Init().Result([]interface{}{
 		field.Text("作者").SetValue("tangtanglove"),
 		field.Text("联系方式").SetValue("dai_hang_love@126.com"),
-		field.Text("官方网址").SetValue("<a href='https://www.qasl.cn' target='_blank'>www.qasl.cn</a>"),
+		field.Text("官方网址").SetValue("<a href='https://www.quarkcms.com' target='_blank'>www.quarkcms.com</a>"),
 		field.Text("文档地址").SetValue("<a href='https://www.quarkcms.com' target='_blank'>查看文档</a>"),
 		field.Text("BUG反馈").SetValue("<a href='https://github.com/quarkcms/quark-go/issues' target='_blank'>提交BUG</a>"),
 		field.Text("代码仓储").SetValue("<a href='https://github.com/quarkcms/quark-go' target='_blank'>Github</a>"),
