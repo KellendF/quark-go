2b07dc938a8fa19a6539f07cf7cbee23cf56c197
tangtanglove <dai_hang_love@126.com>
2022-07-12 16:41:56 +0800

    fix

--- internal/console/commands/install.go
+++ internal/console/commands/install.go
@@ -2,6 +2,12 @@ package commands
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/fatih/color"
+	"github.com/quarkcms/quark-go/database"
+	"github.com/quarkcms/quark-go/pkg/ui/admin/utils"
 )
 
 type Install struct {
@@ -11,12 +17,43 @@ type Install struct {
 // 初始化
 func (p *Install) Init() *Install {
 	p.Signature = "install"
-	p.Description = "The Command Description"
+	p.Description = ""
 
 	return p
 }
 
 // 执行命令
 func (p *Install) Handle() {
-	fmt.Println("Execute Command")
+
+	// 如果锁定文件存在则不执行安装步骤
+	if utils.PathExist("install.lock") {
+		color.Set(color.FgRed)
+		fmt.Println("Install failed : The lock file exists in the root path, please delete it!")
+		color.Unset()
+
+		return
+	}
+
+	// 创建软连接
+	storagePath := filepath.Join("..", "storage", "app", "public")
+	SymlinkPath := filepath.Join("public", "storage")
+
+	err := os.Symlink(storagePath, SymlinkPath)
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	// 执行迁移
+	(&database.Migrate{}).Handle()
+
+	// 数据填充
+	(&database.Seed{}).Handle()
+
+	// 创建锁定文件
+	file, _ := os.Create("install.lock")
+	file.Close()
+
+	color.Set(color.FgGreen)
+	fmt.Println("The application installed successfully")
+	color.Unset()
 }
--- internal/http/kernel.go
+++ internal/http/kernel.go
@@ -1,31 +1,21 @@
 package http
 
 import (
-	"fmt"
 	"io/fs"
 	"net/http"
-	"os"
-	"path/filepath"
 	"strings"
 	"time"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/filesystem"
-	"github.com/quarkcms/quark-go/database"
 	"github.com/quarkcms/quark-go/internal/http/middleware"
 	"github.com/quarkcms/quark-go/internal/providers"
 	"github.com/quarkcms/quark-go/pkg/framework/config"
-	"github.com/quarkcms/quark-go/pkg/ui/admin/utils"
 )
 
 type Kernel struct{}
 
 func (p *Kernel) Run(assets fs.FS) {
-
-	if config.Get("app.debug").(string) == "false" {
-		// 安装
-		p.install()
-	}
 
 	// 配置
 	app := fiber.New(fiber.Config{
@@ -69,32 +59,4 @@ func (p *Kernel) Run(assets fs.FS) {
 	}))
 
 	app.Listen(config.Get("app.host").(string))
-}
-
-// 安装操作
-func (p *Kernel) install() {
-
-	// 如果锁定文件存在则不执行安装步骤
-	if utils.PathExist("install.lock") {
-		return
-	}
-
-	// 创建软连接
-	storagePath := filepath.Join("..", "storage", "app", "public")
-	SymlinkPath := filepath.Join("public", "storage")
-
-	err := os.Symlink(storagePath, SymlinkPath)
-	if err != nil {
-		fmt.Print(err)
-	}
-
-	// 执行迁移
-	(&database.Migrate{}).Handle()
-
-	// 数据填充
-	(&database.Seed{}).Handle()
-
-	// 创建锁定文件
-	file, _ := os.Create("install.lock")
-	file.Close()
 }
